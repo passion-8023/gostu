d8380d7ff91741765425da17d422ff3407ecc3ef
wangxin <wangxin>
2021-10-12 19:04:14 +0800

    kmp算法

--- app/controllers/strMatch/strMatch.go
+++ app/controllers/strMatch/strMatch.go
@@ -127,3 +127,46 @@ func bmIndexOf(S, T string) int {
 	return -1
 }
 
+func KnuthMorrisPratt(ctx *gin.Context)  {
+	a := "ababaeababc"
+	b := "ababacd"
+	index := kmpIndexOf(a, b)
+	response.SuccessResponse(ctx, "获取成功", index)
+}
+
+func kmpIndexOf(S, T string) int {
+	next := getNext(T)
+	lenS := len(S)
+	lenT := len(T)
+	j := 0
+	for i := 0; i < lenS; i++ {
+		for j > 0 && S[i] != T[j] {
+			j = next[j-1] + 1
+		}
+		if S[i] == T[j] {
+			j++
+		}
+		if j == lenT {
+			return i - lenT + 1
+		}
+	}
+	return -1
+}
+
+func getNext(T string) []int {
+	lenT := len(T)
+	next := make([]int, lenT)
+	next[0] = -1
+	k := -1
+	for i := 1; i < lenT; i++ {
+		for k != -1 && T[k+1] != T[i] {
+			k = next[k]
+		}
+		if T[k+1] == T[i] {
+			k++
+		}
+		next[i] = k
+	}
+	return next
+}
+
